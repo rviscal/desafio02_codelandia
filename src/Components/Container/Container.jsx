802357759ac446f92bceb5636f51dd46d73d9961
Atendimento <atendimento@mobiohsistemas.com.br>
2023-02-09 10:12:10 -0300

    Inicio novo desafio

---
+++ src/Components/Container/Container.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Boddy,Promo,PromoText } from './containerStyle';
+
+function Container() {
+    return (
+        <>
+            <Boddy>
+                <Promo>
+                    <PromoText>Ganhe R$ 10,00 de desconto no frete</PromoText>
+                </Promo>
+            </Boddy>
+        </>
+    )
+}
+export default Container;
---
+++ src/Components/Container/containerStyle.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Boddy = styled.body`
+width: 1920px;
+height: 1515px;
+background: #F9F9F9;
+`
+export const Promo = styled.div`
+display:flex;
+align-items:center;
+flex-direction:column;
+width: 1920px;
+height: 60px;
+background: #181818;
+`
+export const PromoText = styled.p`
+color:#F9F9F9;
+font-size: 16px
+`
