dc17a7e6d4c276496674e3cedcd0602fb709597c
Atendimento <atendimento@mobiohsistemas.com.br>
2023-02-09 14:49:59 -0300

    import imagens

---
+++ src/Components/Container/DisplaysContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ImgData } from "./ImgData";
+import {Container} from "./DisplaysContainerStyle"
+
+
+function DisplaysContainer (){
+    return(
+        <Container>
+        
+        </Container>
+    )
+
+}
